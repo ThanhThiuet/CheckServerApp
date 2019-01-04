7522e53594ba160afe1ff09f52898357da39a7cb
Nguyen Thanh Thi <thanhthiuet@gmail.com>
2019-01-04 18:29:55 +0700

    update layout create or edit item

---
+++ CheckServer/app/src/main/java/com/example/thanhthi/checkserver/updateItem/EmptyViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.thanhthi.checkserver.updateItem;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class EmptyViewHolder extends RecyclerView.ViewHolder
+{
+    public EmptyViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
---
+++ CheckServer/app/src/main/java/com/example/thanhthi/checkserver/updateItem/FooterViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.thanhthi.checkserver.updateItem;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class FooterViewHolder extends RecyclerView.ViewHolder
+{
+    public FooterViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
